3f1939703eb620bbd9f09e13bfe4c1fb7c54955a
IS97264 <IS97264@T1LPRVT3081>
2021-01-10 06:04:00 +0300

    Sadece Unit test projesi ekledim ve bana gönderilen örnek unit test classını da tekrar elle yazdım ki anlayabileyim. Test implement etmedim.

---
+++ UnitTestProject1/CustomUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //Example Run
+        int expectedVal = 5;
+        int myCalculatedVal = 5; //How am I going to take this? By consuming the API?
+
+        EvaluateTestResult(1, expectedVal == myCalculatedVal);
+    }
+
+    private static void EvaluateTestResult(int testCaseNumber, bool compareResult)
+    {
+        string result = compareResult ? "SUCCESS" : "FAIL";
+        Console.WriteLine("TestCase" + testCaseNumber + " : " + result);
+    }
+}
+
